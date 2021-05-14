300d593756e7ede41b2e074cee5a4965dabf8989
lier <livia.erdelyi@zuehlke.com>
2021-05-14 20:50:06 +0200

    Api urls

--- src/app/spot.service.ts
+++ src/app/spot.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Spot} from './spot.model';
 import {ImageSource} from './imagesource.model';
+import {environment} from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -12,19 +13,19 @@ export class SpotService {
   }
 
   public getSpots() {
-    return this.http.get<Spot[]>('http://localhost:8080/getAllSpots');
+    return this.http.get<Spot[]>(environment.apiUrl + '/getAllSpots');
   }
 
   public createSpot(spot) {
-    return this.http.post<Spot>('http://localhost:8080/saveSpot', spot);
+    return this.http.post<Spot>(environment.apiUrl + '/saveSpot', spot);
   }
 
   public getSpotById(id: number) {
-    return this.http.get<Spot>('http://localhost:8080/getSpot/' + id);
+    return this.http.get<Spot>(environment.apiUrl + '/getSpot/' + id);
   }
 
   // TODO make its own service
   public getImagesBySpotId(spotId: string) {
-    return this.http.get<ImageSource[]>('http://localhost:8080/getImages/' + spotId);
+    return this.http.get<ImageSource[]>(environment.apiUrl + '/getImages/' + spotId);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: '',
+  baseHref: 'https://erdelyilivi.github.io/pet-ui/'
 };
